c5c15a38f3da36d341ea4ff1184d0eb0346e232d
shy1 <sethyoungzip@gmail.com>
2017-11-16 07:59:44 -0500

    lots of changes

--- cupy-hypervoir.py
+++ cupy-hypervoir.py
@@ -1,12 +1,19 @@
-# Reservoir computing on the hypersphere
+## Reservoir computing on the hypersphere
 import numpy as np
 import cupy as cp
 import chargrams as cg
 import re
-#import csv
+import pickle
 import time
+
 #from sklearn.metrics import log_loss
 from gensim.models.keyedvectors import KeyedVectors
+
+import pydybm.arraymath as amath
+import pydybm.arraymath.dycupy as dycupy
+from pydybm.base.sgd32 import ADAM
+
+
 
 def gramsintext(text, n=2):
     grams = cg.chargrams(text, n)
@@ -18,35 +25,54 @@ def gramsintext(text, n=2):
 
 # create input weight matrix u and output value one-hot identity matrix(?) v
 def init(M, N):
-    nu = np.empty((M, M), dtype=np.float32)
-    ru = cp.random.rand(N,M, dtype=np.float32)
+#    nu = np.empty((N, M), dtype=np.float32)
+    ui = cp.random.rand(N * layerscales["L1"], M, dtype=np.float32)
+    # u1 = cp.random.rand(N,M, dtype=np.float32)
+    u2 = cp.random.rand(N * layerscales["L2"], M, dtype=np.float32)
+    # u3 = cp.random.rand(N,M, dtype=np.float32)
+    u4 = cp.random.rand(N * layerscales["L3"], M, dtype=np.float32)
     v = cp.identity(M, dtype=np.float32)
     # normalizing columns in NxM sized input matrix U as in formulas 6, 7
-    wv = KeyedVectors.load_word2vec_format('/home/user01/dev/wang2vec/embeddings2-i3e4-ssg-neg15-s1024w4.txt', binary=False)
+#    wv = KeyedVectors.load_word2vec_format('/home/user01/dev/wang2vec/embeddings-i3e4-ssg-neg15-s512w9.txt', binary=False)
+    wv = KeyedVectors.load_word2vec_format('/home/user01/dev/wang2vec/embeddings-i3e4-ssg-neg15-s1024w6.txt', binary=False)
     temp = wv.index2word
     glist = np.array(temp[1:len(temp)])
 #    print(len(arr))
-    print(glist[1022])
 #    print(wv['e_'])
-    
-    for i in range(0, M):        
-#        nu[:, i] = wv[glist[i]]
-        temp = glist[i]
-        nu[:, i] = wv.word_vec(temp, use_norm=False)
+
+#    for i in range(0, M):
+#        temp = glist[i]
+#        nu[:, i] = wv.word_vec(temp, use_norm=False)
 
     #print(nu.shape, nu)
-    u = cp.asarray(nu)
-#    u = ru
+#    u = cp.asarray(nu)
+
     for m in range(M):
-        u[:, m] = u[:, m] - u[:, m].mean()
-        u[:, m] = u[:, m] / cp.linalg.norm(u[:, m])
-    
+        ui[:, m] = ui[:, m] - ui[:, m].mean()
+        ui[:, m] = ui[:, m] / cp.linalg.norm(ui[:, m])
+
+    # for m in range(M):
+    #     u1[:, m] = u1[:, m] - u1[:, m].mean()
+    #     u1[:, m] = u1[:, m] / cp.linalg.norm(u1[:, m])
+
+    for m in range(M):
+        u2[:, m] = u2[:, m] - u2[:, m].mean()
+        u2[:, m] = u2[:, m] / cp.linalg.norm(u2[:, m])
+
+    # for m in range(M):
+    #     u3[:, m] = u3[:, m] - u3[:, m].mean()
+    #     u3[:, m] = u3[:, m] / cp.linalg.norm(u3[:, m])
+
+    for m in range(M):
+
+        u4[:, m] = u4[:, m] - u4[:, m].mean()
+        u4[:, m] = u4[:, m] / cp.linalg.norm(u4[:, m])
     #print(u.shape, u)
     glist = [re.sub(r'_', ' ', j) for j in glist]
     #print(glist)
-    return u, v, glist
+    return ui, u2, u4, v, glist
 
-def grecall(T, N, w, u, c, a, ss):
+def grecall(T, N, w, u, a, ss):
     x, i = cp.zeros(N, dtype=np.float32), ss
     ssa = []
     ssa.append(ss)
@@ -58,6 +84,29 @@ def grecall(T, N, w, u, c, a, ss):
         ssa.append(i)
     return ssa
 
+def generate(T, N, u, variables, a, s0, temp=0.5):
+    x, i = cp.zeros(N, dtype=np.float32), s0
+    ssa = []
+    ssa.append(s0)
+
+    for t in range(T - 1):
+        x = (1.0 - a) * x + a * (u[:, i] + cp.roll(x, 1))
+        x = x / cp.linalg.norm(x)
+        # probability distribution computed same as in online training
+        # except that output of dot(w, x) is divided by the temperature
+        output = cp.dot(variables["W1"], x) / temp
+        output = output - np.max(output)
+        probs = cp.exp(output)
+        probs = probs / cp.sum(probs)
+        i = cp.argmax(cp.random.multinomial(1, probs))
+        ssa.append(i)
+
+    sstext = ""
+    for ssi in range(0, len(ssa), 2):
+        #print(ssi, type(ssi))
+        sstext += glist[int(ssa[ssi])]
+    print(sstext)
+
 def error(s, ss):
     err = 0.
     for t in range(len(s)):
@@ -66,37 +115,46 @@ def error(s, ss):
     totalerr = err*100.0 / len(s)
     return totalerr
 
-def online_grams(u, v, c, a, s):
+def online_grams(u, v, w, a, s):
     T, (N, M) = len(s), u.shape
-    w = cp.zeros((M, N), dtype=np.float32)
     err = 100
     tt = 0
+
     #total = time.clock()
     totalp = time.perf_counter()
     while err > 0 and tt < T:
         x = cp.zeros(N, dtype=np.float32)
-
+        softerr = 0
         for t in range(T - 1):
             #start = time.clock()
             #startp = time.perf_counter()
             x = (1.0 - a) * x + a * (u[:, s[t]] + cp.roll(x, 1))
             x = x / cp.linalg.norm(x)
             p = cp.exp(cp.dot(w, x))
+#            psum = cp.sum(p)
             p = p / cp.sum(p)
 #            print(p[0:19])
 #            print(v[0:19, s[t+1]])
             smax_idx = cp.argmax(p)
             smax_prob = p[smax_idx]
-#            print(tv)
+            softerr += 1 - smax_prob
             w = w + cp.outer(v[:, s[t+1]] - p, x)
-        ssa = grecall(T, N, w, u, c, a, s[0])
+        avgerr = softerr / (T - 1)
+        ssa = grecall(T, N, w, u, a, s[0])
+        ssg = generate(100, N, u, a, s[0], s[1])
         err = error(s, ssa)
+
         tt = tt + 1
         if tt % 3 == 0:
-#            a = a * 0.985
+#            a = a * 0.98
             #elapsed = time.clock() - start
             #elapsedp = time.perf_counter() - startp
-            print(tt, "err=", err, "%", "alpha:", a, "smax err:", 1 - smax_prob)
+            print(tt, "err:", err, "%", "softavg:", avgerr, "alpha:", a)
+            sstext = ""
+            for ssi in range(0, len(ssg), 2):
+                #print(ssi, type(ssi))
+                sstext += glist[int(ssg[ssi])]
+            print(sstext)
     sstext = ""
     #endtotal = time.clock() - total
     endtotalp = time.perf_counter() - totalp
@@ -104,6 +162,11 @@ def online_grams(u, v, c, a, s):
         #print(ssi, type(ssi))
         sstext += glist[int(ssa[ssi])]
     print(tt, "err=", err, "%\n", sstext, "\n", endtotalp)
+    sstext = ""
+    for ssi in range(0, len(ssg), 2):
+        #print(ssi, type(ssi))
+        sstext += glist[int(ssg[ssi])]
+    print(sstext)
     return ssa, w
 
 def offline_grams(u, v, c, a, s):
@@ -124,68 +187,320 @@ def offline_grams(u, v, c, a, s):
     w = cp.dot(cp.dot(S, X.T), cp.linalg.inv(XX))
     ssa = grecall(T, N, w, u, c, alpha, s[0])
 
-    for ssi in ssa:
-        sstext += glist[int(ssi)]
+    sstext = ""
+    for ssi in range(0, len(ssa), 2):
+        sstext += glist[int(ssa[ssi])]
     print("err=", error(s, ssa), "%\n", sstext, "\n")
     return ssa, w
 
+def train(ui, u2, u4, v, variables, leaks, s):
+    gradient = dict()
+
+    # bs = batch size
+    bs = 32
+    T = len(s)
+    N = 1024
+    M = 1024
+    #total = time.clock()
+    #totalp = time.perf_counter()
+    x1 = cp.zeros(N * layerscales["L1"], dtype=np.float32)
+    # x2 = cp.zeros(N, dtype=np.float32)
+    x3 = cp.zeros(N * layerscales["L2"], dtype=np.float32)
+    # x4 = cp.zeros(N, dtype=np.float32)
+    x5 = cp.zeros(N * layerscales["L3"], dtype=np.float32)
+    # print("x1: {} x3: {} x5: {}".format(x1.shape, x3.shape, x5.shape))
+
+    softerr1 = 0
+    err1 = 0
+    softerr3 = 0
+    err3 = 0
+    softerr5 = 0
+    err5 = 0
+    softerrm = 0
+    errm = 0
+#    print(x1.shape, variables["W1"].shape)
+    tm1 = (T - 1)
+
+    # floored quotient (integer without remainder)
+    fullbatches = tm1 // bs
+    lastlen = tm1 - (fullbatches * bs)
+    t = 0
+    batchgrads1 = cp.empty(bs, dtype=np.float32)
+    batchgrads3 = cp.empty(bs, dtype=np.float32)
+    batchgrads5 = cp.empty(bs, dtype=np.float32)
+    lastgrads1 = cp.empty(lastlen, dtype=np.float32)
+    lastgrads3 = cp.empty(lastlen, dtype=np.float32)
+    lastgrads5 = cp.empty(lastlen, dtype=np.float32)
+    # print("batch: {} last:{}".format(batchgrads1.shape, lastgrads1.shape))
+    for b in range(fullbatches):
+
+        for i in range(bs):
+            current = s[t]
+
+            x1 = (1.0 - leaks[0]) * x1 + leaks[0] * (ui[:, current] + cp.roll(x1, 1))
+            x1 = x1 / cp.linalg.norm(x1)
+            wx = cp.dot(variables["W1"], x1)
+            wx = wx - cp.max(wx)
+            p = cp.exp(wx)
+            p1 = p / cp.sum(p)
+
+            pu2 = cp.dot(p1, u2.T)
+            x3 = (1.0 - leaks[1]) * x3 + leaks[1] * (pu2 + cp.roll(x3, 1))
+            x3 = x3 / cp.linalg.norm(x3)
+            wx = cp.dot(variables["W3"], x3)
+            wx = wx - cp.max(wx)
+            p = cp.exp(wx)
+            p3 = p / cp.sum(p)
+
+            pu4 = cp.dot(p3, u4.T)
+            x5 = (1.0 - leaks[2]) * x5 + leaks[2] * (pu4 + cp.roll(x5, 1))
+            x5 = x5 / cp.linalg.norm(x5)
+            wx = cp.dot(variables["W5"], x5)
+            wx = wx - cp.max(wx)
+            p = cp.exp(wx)
+            p5 = p / cp.sum(p)
+            pred5 = cp.argmax(p5)
+
+            # pstack = cp.stack((p1, p3, p5), axis=1)
+            # print(variables["Ws"].shape, pstack.shape)
+
+            # wx = cp.dot(variables["Ws"], wxstack)
+            # # wx = cp.dot(pstack, variables["Ws"])
+            # # print(wx.shape)
+            # wx = wx - cp.max(wx)
+            # p = cp.exp(wx)
+            # pm = p / cp.sum(p)
+            # meanpred = cp.argmax(pm)
+
+            target = s[t+1]
+
+            target_prob1 = p1[target]
+            target_prob3 = p3[target]
+            target_prob5 = p5[target]
+            err5 = err5 + (pred5 != target)
+
+            softerr1 += 1 - target_prob1
+            softerr3 += 1 - target_prob3
+            softerr5 += 1 - target_prob5
+
+            batchgrads1[i] = cp.outer(v[:, target] - p1, x1)
+            batchgrads3[i] = cp.outer(v[:, target] - p3, x3)
+            batchgrads5[i] = cp.outer(v[:, target] - p5, x5)
+
+            t += 1
+
+        gradient["W1"] = batchgrads1.mean()
+        gradient["W3"] = batchgrads3.mean()
+        gradient["W5"] = batchgrads5.mean()
+        SGD.update_state(gradient)
+        delta = SGD.get_delta()
+        SGD.update_with_L1_regularization(variables, delta, L1)
+        # SGD.apply_L2_regularization(gradient, variables, L2)
+    for j in range(lastlen):
+        current = s[t]
+        x1 = (1.0 - leaks[0]) * x1 + leaks[0] * (ui[:, current] + cp.roll(x1, 1))
+        x1 = x1 / cp.linalg.norm(x1)
+        wx = cp.dot(variables["W1"], x1)
+
+        wx = wx - cp.max(wx)
+        p = cp.exp(wx)
+        p1 = p / cp.sum(p)
+
+        pu2 = cp.dot(p1, u2.T)
+        x3 = (1.0 - leaks[1]) * x3 + leaks[1] * (pu2 + cp.roll(x3, 1))
+        x3 = x3 / cp.linalg.norm(x3)
+        wx = cp.dot(variables["W3"], x3)
+        wx = wx - cp.max(wx)
+        p = cp.exp(wx)
+        p3 = p / cp.sum(p)
+
+        pu4 = cp.dot(p3, u4.T)
+        x5 = (1.0 - leaks[2]) * x5 + leaks[2] * (pu4 + cp.roll(x5, 1))
+        x5 = x5 / cp.linalg.norm(x5)
+        wx = cp.dot(variables["W5"], x5)
+        wx = wx - cp.max(wx)
+        p = cp.exp(wx)
+        p5 = p / cp.sum(p)
+        pred5 = cp.argmax(p5)
+
+        target = s[t+1]
+        target_prob1 = p1[target]
+        target_prob3 = p3[target]
+        target_prob5 = p5[target]
+        err5 = err5 + (pred5 != target)
+
+        softerr1 += 1 - target_prob1
+        softerr3 += 1 - target_prob3
+        softerr5 += 1 - target_prob5
+
+        lastgrads1[j] = cp.outer(v[:, target] - p1, x1)
+        lastgrads3[j] = cp.outer(v[:, target] - p3, x3)
+        lastgrads5[j] = cp.outer(v[:, target] - p5, x5)
+
+        t += 1
+    if lastlen > 0:
+        gradient["W1"] = lastgrads1.mean()
+        gradient["W3"] = lastgrads3.mean()
+        gradient["W5"] = lastgrads5.mean()
+        SGD.update_state(gradient)
+        delta = SGD.get_delta()
+        SGD.update_with_L1_regularization(variables, delta, L1)
+
+    softerrors = dict()
+    prederrors = dict()
+    softerrors["lay1"] = softerr1 / (tm1)
+    softerrors["lay3"] = softerr3 / (tm1)
+    softerrors["lay5"] = softerr5 / (tm1)
+    # softerrors["laym"] = softerrm / (tm1)
+    # prederrors["lay1"] = err1 * 100.0 / (tm1)
+    # prederrors["lay3"] = err3 * 100.0 / (tm1)
+    prederrors["lay5"] = err5 * 100.0 / (tm1)
+    # prederrors["laym"] = errm * 100.0 / (tm1)
+    return prederrors, softerrors, variables
+
 
 n=2
-with open('test02.txt', 'r') as ofile:
-    s = ofile.read()
-
-glist = []
-#with open('counts1024b.txt', 'r') as countfile:
-#    counts = countfile.readlines()
-#for line in counts:
-#    glist.append(line[:2])
-#gramindex = {gram:idx for idx, gram in enumerate(glist)}
-#print(len(gramindex), type(gramindex))
-
-unclean = False
-sgi = []
 stride = 1
 
-s = re.sub('\n', ' ', s)
-if unclean:
-    s = cg.gut_clean(s)
 
-cp.random.seed(11712)
+#s = re.sub('\n', ' ', s)
+#if unclean:
+#    s = cg.gut_clean(s)
 
+cp.random.seed(481639)
+## alpha of .4452 achieved 0.0 err with offline grams using 1-d512-w9 and 946 char document
+
+#alpha = .369
+#alpha = .4452
+#alpha = .467
+#alpha = .487
+# alpha = .52037
+#alpha = .537
+#alpha = .58
+#alpha = 0.6301
+
+# leaks = [0.8155, 0.6309, 0.3155]
+# leaks = [0.3155, 0.6309, 0.8155]
+leaks = [0.6905, 0.5655, 0.4405]
+# leaks = [0.6905, 0.581, 0.4405]
+# leaks = [0.4405, 0.5655, 0.6905]
+# leaks = [0.73814, .52037, 0.3679]
+# leaks = [0.73814, .58078, 0.43178]
 N = 1024
+#N = 1024
 M = 1024
+layerscales = dict()
+layerscales["L1"] = 3
+layerscales["L2"] = 2
+layerscales["L3"] = 3
 
-u, v, glist = init(M, N)
+amath.setup(dycupy)
+lrate = 0.0025
+SGD = ADAM(alpha=lrate)
+
+variables = dict()
+variables["W1"] = cp.zeros((M, N * layerscales["L1"]), dtype=np.float32)
+variables["W3"] = cp.zeros((M, N * layerscales["L2"]), dtype=np.float32)
+variables["W5"] = cp.zeros((M, N * layerscales["L3"]), dtype=np.float32)
+# variables["Ws"] = cp.zeros((1024, N*3), dtype=np.float32)
+# variables["Ws"] = cp.array([0.002, 0.003, 0.005], dtype=np.float32)
+SGD = SGD.set_shape(variables)
+print("Learning rate:", SGD.alpha)
+L2 = dict()
+L1 = dict()
+for key in variables:
+    L1[key] = 0
+    L2[key] = 0
+
+ui, u2, u4, v, glist = init(M, N)
 gramindex = {gram:idx for idx, gram in enumerate(glist)}
-print(len(gramindex), type(gramindex))
-print(u.shape, v.shape)
+# print(len(gramindex), type(gramindex))
+print("ui: {} u2: {} u4: {}".format(ui.shape, u2.shape, u4.shape))
+print("W1: {} W3: {} W5: {}".format(variables["W1"].shape, variables["W3"].shape, variables["W5"].shape))
 
-for idx in range(0, len(s) - (n - 1), stride):
-    sgi.append(gramindex[s[idx:idx + n]])
-print(len(sgi))
+chunkfile = '/home/user01/dev/language-model/chunks100mb.p'
+outweights = '/home/user01/dev/language-model/outweights100mb.p'
+inweights = '/home/user01/dev/language-model/inweights100mb.p'
+#pickle.dump(allchunks, open(outfile, "wb"))
+chunklist = pickle.load(open(chunkfile, "rb"))
+print(len(chunklist))
+intchunklist = []
 
-T, M = len(sgi), len(glist)
-mt = M / T
+for j in range(33):
+    chunk = chunklist[j]
+    # pad with space if chunk has an odd number of characters so that
+    # chunk length is compatibile with bigrams using a stride of 2
+    if len(chunk) % 2 != 0:
+        chunk += " "
+    sgi = []
+    for idx in range(0, len(chunk) - (n - 1), stride):
+        try:
+            sgi.append(gramindex[chunk[idx:idx + n]])
+        except:
+            print(chunk[idx:idx + n])
+    intchunk = cp.asarray(sgi, dtype=np.int16)
+    intchunklist.append(intchunk)
+
+#for i in range(len(chunklist))
+print(leaks[0], leaks[1], leaks[2])
+totalstart = time.perf_counter()
+for i in range(99):
+    startp = time.perf_counter()
+    totalerr1 = 0
+    totalerr3 = 0
+    totalerr5 = 0
+    totalerrm = 0
+    for chunk in intchunklist:
+        prederrs, softerrs, variables = train(ui, u2, u4, v, variables, leaks, chunk)
+        totalerr1 += softerrs["lay1"]
+        totalerr3 += softerrs["lay3"]
+        totalerr5 += softerrs["lay5"]
+        # totalerrm += softerrs["laym"]
+#        if i % 100 == 0:
+    elapsedp = time.perf_counter() - startp
+    totalelapsed = time.perf_counter() - totalstart
+    tm, ts = divmod(totalelapsed, 60)
+    totalerr1 = totalerr1 / 33
+    totalerr3 = totalerr3 / 33
+    totalerr5 = totalerr5 / 33
+    # totalerrm = totalerrm / 33
+    print("\n", i, elapsedp, "-- {0:.0f}m {1:.0f}s".format(tm, ts))
+    # print("Errors:", prederrs["lay1"], prederrs["lay3"], prederrs["lay5"])
+    print("Errors:", prederrs["lay5"])
+    print("Losses:", totalerr1, totalerr3, totalerr5)
+    # print("Losses:", totalerr5)
+    # generate(128, N, u, variables, alpha, 4)
+
+pickle.dump(variables, open(outweights, "wb"))
+pickle.dump(ui, open(inweights, "wb"))
+
+
+#    if tt % 10 == 0:
+##        a = a * 0.98
+#
+#        print(tt, "err:", err, "%", "softavg:", avgerr, "alpha:", a)
+#        sstext = ""
+#        for ssi in range(0, len(ssg), 2):
+#            #print(ssi, type(ssi))
+#            sstext += glist[int(ssg[ssi])]
+#        print(sstext)
+
+
 #N = int(M * 2.71828)
+#div = ' mt:nt '
+#nt = N / T
+#nmt = [str(mt), str(nt)]
+#print(div.join(nmt))
+#print(T, N, M, alpha)
+#print(T, N, M, alpha)
 
-#alpha = .52037
-alpha = 0.6301
-#alpha = np.log(2) / np.log(3)
-div = ' mt:nt '
-nt = N / T
-nmt = [str(mt), str(nt)]
-print(div.join(nmt))
-print(T, N, M, alpha)
+#ss, w = offline_grams(u, v, glist, alpha, sgi)
 
 # totalo = time.perf_counter()
-# ss, w = offline_grams(u, v, glist, alpha, sgi)
 # endtotalo = time.perf_counter() - totalo
 # print(endtotalo)
 #cu_glist = cp.asarray(glist, dtype=np.float32)
 #cu_sgi = cp.asarray(sgi, dtype=np.int16)
-
-ss, w = online_grams(u, v, glist, alpha, sgi)
-print(T, N, M, alpha)
 
 # tnt = int(T * 2)
 # if tnt > M:
@@ -203,3 +518,16 @@ print(T, N, M, alpha)
 # alpha is determined by ratio nt (hidden states) minus ratio mt (input states)
 # u = input weight matrix
 # v = hidden state orthogonal identity matrix
+#with open('test02.txt', 'r') as ofile:
+#    s = ofile.read()
+
+#with open('counts1024b.txt', 'r') as countfile:
+#    counts = countfile.readlines()
+#for line in counts:
+#    glist.append(line[:2])
+#gramindex = {gram:idx for idx, gram in enumerate(glist)}
+#print(len(gramindex), type(gramindex))
+
+#unclean = False
+
+#number of characters in each n-gram sequence that forms a single unit/timestep
--- temp02.py
+++ temp02.py
@@ -1,29 +1,65 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Oct 31 01:29:36 2017
 
-@author: user01
-"""
+import glob
+import pickle
+import re
+from random import shuffle
 
-from spacy.en import English
-parser = English()
 
-file_name = "/home/user01/dev/data/gutenberg/cleaner/Anthony Trollope___Kept in the Dark.txt"
-with open(file_name) as f:
-    raw_text = f.read()
 
-parsedData = parser(raw_text)
+def findFiles(path): return glob.glob(path)
 
-sents = []
+inputpath='/home/user01/dev/data/gutenberg/nochapters-nl'
+inputpath = inputpath + '/*.txt'
+allchunks = []
 
-for span in parsedData.sents:
-    # go from the start to the end of each span, returning each token in the sentence
-    # combine each token using join()
-    sent = ''.join(parsedData[i].string for i in range(span.start, span.end)).strip()
-    sents.append(sent)
 
-for i in range(50, 80):
-    print(i, len(sents[i]), sents[i])
+for filename in findFiles(inputpath):
+    with open(filename, "r") as f:
+        raw_text = f.read()
 
-print(len(sents))
+    raw_text = raw_text.strip()
+    splits = raw_text.split(". ")
+    chunk = ""
+
+    for i in range(len(splits)):        
+        chunk += " " + splits[i] + "."
+        if len(chunk) > 512:
+#            print(len(chunk))
+            allchunks.append(chunk)
+            chunk = ""
+    print(len(allchunks))
+
+#print(len(allchunks[4]), allchunks[4])
+#print(len(allchunks[9]), allchunks[9])
+print(len(allchunks[1700]), allchunks[1700])
+shuffle(allchunks)
+print('\nchunk [1700] after shuffling')
+print(len(allchunks[1700]), allchunks[1700])
+
+outfile = '/home/user01/dev/language-model/chunks100mb.p'
+
+pickle.dump(allchunks, open(outfile, "wb"))
+
+filechunks = pickle.load(open(outfile, "rb"))
+
+for i in range(len(filechunks)):
+    filechunks[i] = re.sub('*', ' ', filechunks[i])
+print('\nchunk [1700] after loading from pickled file')
+print(len(filechunks[1700]), filechunks[1700])
+
+#parsedData = parser(raw_text)
+#
+#sents = []
+#
+#for span in parsedData.sents:
+#    # go from the start to the end of each span, returning each token in the sentence
+#    # combine each token using join()
+#    sent = ''.join(parsedData[i].string for i in range(span.start, span.end)).strip()
+#    sents.append(sent)
+#
+#for i in range(50, 80):
+#    print(i, len(sents[i]), sents[i])
+#
+#print(len(sents))
